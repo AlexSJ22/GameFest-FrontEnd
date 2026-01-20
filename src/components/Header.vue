eb6a3006a302011871706b4d9e3f5e6e63ee46a8
in2dw3 <in2dw3@elorrieta.int>
2026-01-20 19:39:28 +0100

    Router | Añadido Views | Componentes - Header | Import Tailwind

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="w-full p-4 flex justify-around bg-purple-400 items-center">
+        <div>
+            <h1 class="text-xl font-bold text-purple-900">GameFest</h1>
+        </div>
+
+        <ul class="flex gap-5 text-white">
+            <li>
+                <RouterLink to="/">Inicio</RouterLink>
+            </li>
+
+            <li>
+                <RouterLink to="/games">Juegos</RouterLink> </li>
+
+            <li>
+                <RouterLink to="/events">Eventos</RouterLink> </li>
+
+            
+            <li>
+                <RouterLink to="/login">Iniciar Sesión</RouterLink> </li>
+        </ul>
+    </nav>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,53 @@
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+
+    {
+      path: '/games',
+      name: 'games',
+      component: () => import('../views/GamesView.vue')
+    },
+
+    {
+      path: '/games/:id',
+      name: 'game-detail',
+      component: () => import('../views/GameDetail.vue')
+    },
+
+    {
+      path: '/events',
+      name: 'events',
+      component: () => import('../views/EventsView.vue')
+    },
+
+    {
+      path: '/events/:id',
+      name: 'event-detail',
+      component: () => import('../views/EventDetail.vue')
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+
+    {
+      path: '/events/create',
+      name: 'create-event',
+      component: () => import('../views/CreateEventView.vue')
+    }
+
+
+  ],
 })
 
 export default router
