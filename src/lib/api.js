7db01d206851e57088f4ea43eaeac3eaa92589ff
AlexSJ22 <alexisjosue.guanosa@elorrieta-errekamari.com>
2026-01-29 16:24:57 +0100

    Filtros funcional 2.0

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost/GameFest-BackEnd',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
