9c99688dc50768c9634704268dcfb977943f5b56
in2dw3 <in2dw3@elorrieta.int>
2026-01-29 18:52:55 +0100

    Login arreglado nuevos hashes

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -10,7 +10,7 @@ export const useAuthStore = defineStore('auth', {
   actions: {
     async login(email, password) {
       // Calls your login.php file
-      const response = await api.post('/auth/login.php', { email, password })
+      const response = await api.post('http://localhost/GameFest-BackEnd/auth/login.php', { email, password } , {withCredentials: true })
       if (response.data.success) {
         this.user = response.data.user
         this.isLoggedIn = true
@@ -20,7 +20,7 @@ export const useAuthStore = defineStore('auth', {
     },
     async register(username, email, password) {
       // Calls your register.php file
-      const response = await api.post('/auth/register.php', { username, email, password })
+      const response = await api.post('http://localhost/GameFest-BackEnd/auth/register.php', { username, email, password } , {withCredentials: true })
       return response.data
     },
     logout() {
